562e42ed3cd3487ba6d31a7bdebcec0ca920b060
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-07-25 21:59:55 +0300

    Initial Commit

---
+++ Project5-Word-Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5-Word-Scramble
+//
+//  Created by Amby on 25/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
